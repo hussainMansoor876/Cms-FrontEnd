495d0574ad01b8011195f3c4ba1e6b4d5986d54b
Mansoor Hussain <hussainmansoor876@gmail.com>
2019-09-18 18:52:21 +0500

    added input fields

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -5,18 +5,45 @@ import SessionStorageManager from '../../Config/SessionStorageManager';
 import { connect } from 'react-redux';
 import { loginUser } from '../../Redux/actions/authActions'
 import 'antd/dist/antd.css';
-import { Menu, Icon, Input, Button, Select } from 'antd';
+import { Menu, Icon, Input, Button, Select, Typography, Form, Checkbox } from 'antd';
 import { Link } from 'react-router-dom';
 
 const { Option } = Select;
 const { Search } = Input;
+const { Title } = Typography
+// const AutoCompleteOption = AutoComplete.Option;
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
 
 
 class Article extends React.Component {
 
 
   state = {
-    current: 'mail',
+    confirmDirty: false,
+    autoCompleteResult: [],
     user: null
   }
 
@@ -45,59 +72,104 @@ class Article extends React.Component {
 
 
   render() {
+    const { getFieldDecorator } = this.props.form;
     const { user } = this.state
+    const prefixSelector = getFieldDecorator('prefix', {
+      initialValue: '86',
+    })(
+      <Select style={{ width: 70 }}>
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>,
+    );
     return (
-      <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ paddingTop: 10, paddingBottom: 10, height: 70, backgroundColor: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} className="right">
-          <Search
-            placeholder="Search by"
-            enterButton="Search"
-            size="large"
-            // style={{ paddingTop: 5 }}
-            className="search1"
-            style={{ paddingTop: 18, paddingBottom: 10, paddingLeft: 5, alignSelf: 'center' }}
-            onSearch={value => console.log(value)}
-          />
-          <Select defaultValue="City" size="large" className="selector" style={{ width: 120, marginLeft: 10, paddingTop: 8 }} onChange={(value) => this.handleChange(value)}>
-            <Option value="city">City</Option>
-            <Option value="categories">Category</Option>
-            <Option value="topics">Topic</Option>
-          </Select>
-        </div>
-        <div style={{ height: 70, backgroundColor: 'white' }} className="left">
+      <div>
+        <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+          <div style={{ paddingTop: 10, paddingBottom: 10, height: 70, backgroundColor: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} className="right">
+            <Search
+              placeholder="Search by"
+              enterButton="Search"
+              size="large"
+              // style={{ paddingTop: 5 }}
+              className="search1"
+              style={{ paddingTop: 18, paddingBottom: 10, paddingLeft: 5, alignSelf: 'center' }}
+              onSearch={value => console.log(value)}
+            />
+            <Select defaultValue="City" size="large" className="selector" style={{ width: 120, marginLeft: 10, paddingTop: 8 }} onChange={(value) => this.handleChange(value)}>
+              <Option value="city">City</Option>
+              <Option value="categories">Category</Option>
+              <Option value="topics">Topic</Option>
+            </Select>
+          </div>
+          <div style={{ height: 70, backgroundColor: 'white' }} className="left">
 
 
-          <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" style={{ display: "flex", justifyContent: 'space-between', alignItems: 'center', paddingTop: 10, paddingBottom: 10 }}>
-            <Menu.Item key="article">
-              <Link to="/home">
-                Add New Article
+            <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" style={{ display: "flex", justifyContent: 'space-between', alignItems: 'center', paddingTop: 10, paddingBottom: 10 }}>
+              <Menu.Item key="article">
+                <Link to="/home">
+                  Add New Article
           </Link>
-            </Menu.Item>
-            <Menu.Item key="gallery">
-              <Link to="/home">
-                Photos-Gallery
+              </Menu.Item>
+              <Menu.Item key="gallery">
+                <Link to="/home">
+                  Photos-Gallery
           </Link>
-            </Menu.Item>
-            {user ?
-              <Menu.Item key="logout" onClick={() => this.logout()}>
-                {/* <Button onClick={() => this.logout()}> */}
+              </Menu.Item>
+              {user ?
+                <Menu.Item key="logout" onClick={() => this.logout()}>
                   <Icon
                     type="arrow-right"
                   />
                   Logout
-              {/* </Button> */}
-              </Menu.Item> : <Menu.Item key="logout" style={{ paddingRight: 30, paddingLeft: 20, height: 70, paddingTop: 10 }}>
-                <Icon
-                  type="arrow-right"
-                />
-                Logout
-              </Menu.Item>}
-          </Menu>
+              </Menu.Item> : null}
+            </Menu>
+          </div>
+        </div>
+        <br />
+        <Title style={{ paddingLeft: 20 }}>Add New Article</Title>
+        <div className='articleForm'>
+          <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+            <Form.Item label="E-mail">
+              {getFieldDecorator('email', {
+                rules: [
+                  {
+                    type: 'email',
+                    message: 'The input is not valid E-mail!',
+                  },
+                  {
+                    required: true,
+                    message: 'Please input your E-mail!',
+                  },
+                ],
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="Phone Number">
+              {getFieldDecorator('phone', {
+                rules: [{ required: true, message: 'Please input your phone number!' }],
+              })(<Input addonBefore={prefixSelector} style={{ width: '100%' }} />)}
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              {getFieldDecorator('agreement', {
+                valuePropName: 'checked',
+              })(
+                <Checkbox>
+                  I have read the <a href="">agreement</a>
+                </Checkbox>,
+              )}
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              <Button type="primary" htmlType="submit">
+                Register
+          </Button>
+            </Form.Item>
+          </Form>
         </div>
       </div>
     )
   }
 }
+
+const WrappedRegistrationForm = Form.create({ name: 'register' })(Article);
 
 
 
@@ -116,4 +188,4 @@ const mapDispatchToProps = (dispatch) => {
 
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Article)
+export default connect(mapStateToProps, mapDispatchToProps)(WrappedRegistrationForm)
