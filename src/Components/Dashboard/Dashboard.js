d7b8b2b7cf20ebd91db9c1d92ea5f8b7b73b0b67
Mansoor HU Hussain <hussainmansoor876@gmail.com>
2019-09-19 22:32:38 +0500

    Added table

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -5,12 +5,36 @@ import SessionStorageManager from '../../Config/SessionStorageManager';
 import { connect } from 'react-redux';
 import { loginUser } from '../../Redux/actions/authActions'
 import 'antd/dist/antd.css';
-import { Menu, Icon, Input, Button, Select } from 'antd';
+import { Menu, Icon, Input, Button, Select, Table } from 'antd';
 import { Link } from 'react-router-dom';
 
 const { Option } = Select;
 const { Search } = Input;
 
+const columns = [
+  {
+    title: 'Headline',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'age',
+  },
+  {
+    title: 'Author',
+    dataIndex: 'address',
+  },
+];
+
+const data = [];
+for (let i = 0; i < 460; i++) {
+  data.push({
+    key: i,
+    name: `Edward King ABc hello rfjygyjgfyh ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
 
 class Dashboard extends React.Component {
 
@@ -45,54 +69,102 @@ class Dashboard extends React.Component {
 
 
   render() {
+    const rowSelection = {
+      hideDefaultSelections: true,
+      selections: [
+        {
+          key: 'all-data',
+          text: 'Select All Data',
+          onSelect: () => {
+            this.setState({
+              selectedRowKeys: [...Array(46).keys()], // 0...45
+            });
+          },
+        },
+        {
+          key: 'odd',
+          text: 'Select Odd Row',
+          onSelect: changableRowKeys => {
+            let newSelectedRowKeys = [];
+            newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+              if (index % 2 !== 0) {
+                return false;
+              }
+              return true;
+            });
+            this.setState({ selectedRowKeys: newSelectedRowKeys });
+          },
+        },
+        {
+          key: 'even',
+          text: 'Select Even Row',
+          onSelect: changableRowKeys => {
+            let newSelectedRowKeys = [];
+            newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+              if (index % 2 !== 0) {
+                return true;
+              }
+              return false;
+            });
+            this.setState({ selectedRowKeys: newSelectedRowKeys });
+          },
+        },
+      ],
+    };
     const { user } = this.state
     return (
-      <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ paddingTop: 10, paddingBottom: 10, height: 70, backgroundColor: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} className="right">
-          <Search
-            placeholder="Search by"
-            enterButton="Search"
-            size="large"
-            // style={{ paddingTop: 5 }}
-            className="search1"
-            style={{ paddingTop: 18, paddingBottom: 10, paddingLeft: 5, alignSelf: 'center' }}
-            onSearch={value => console.log(value)}
-          />
-          <Select defaultValue="City" size="large" className="selector" style={{ width: 120, marginLeft: 10, paddingTop: 8 }} onChange={(value) => this.handleChange(value)}>
-            <Option value="city">City</Option>
-            <Option value="categories">Category</Option>
-            <Option value="topics">Topic</Option>
-          </Select>
-        </div>
-        <div style={{ height: 70, backgroundColor: 'white' }} className="left">
+      <div>
+        <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+          <div style={{ paddingTop: 10, paddingBottom: 10, height: 70, backgroundColor: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} className="right">
+            <Search
+              placeholder="Search by"
+              enterButton="Search"
+              size="large"
+              // style={{ paddingTop: 5 }}
+              className="search1"
+              style={{ paddingTop: 18, paddingBottom: 10, paddingLeft: 5, alignSelf: 'center' }}
+              onSearch={value => console.log(value)}
+            />
+            <Select defaultValue="City" size="large" className="selector" style={{ width: 120, marginLeft: 10, paddingTop: 8 }} onChange={(value) => this.handleChange(value)}>
+              <Option value="city">City</Option>
+              <Option value="categories">Category</Option>
+              <Option value="topics">Topic</Option>
+            </Select>
+          </div>
+          <div style={{ height: 70, backgroundColor: 'white' }} className="left">
 
 
-          <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" style={{ display: "flex", justifyContent: 'space-between', alignItems: 'center', paddingTop: 10, paddingBottom: 10 }}>
-            <Menu.Item key="article">
-              <Link to="/article">
-                Add New Article
+            <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" style={{ display: "flex", justifyContent: 'space-between', alignItems: 'center', paddingTop: 10, paddingBottom: 10 }}>
+              <Menu.Item key="article">
+                <Link to="/article">
+                  Add New Article
           </Link>
-            </Menu.Item>
-            <Menu.Item key="gallery">
-              <Link to="/home">
-                Photos-Gallery
+              </Menu.Item>
+              <Menu.Item key="gallery">
+                <Link to="/home">
+                  Photos-Gallery
           </Link>
-            </Menu.Item>
-            {user ?
-              <Menu.Item key="logout" onClick={() => this.logout()}>
-                {/* <Button onClick={() => this.logout()}> */}
+              </Menu.Item>
+              {user ?
+                <Menu.Item key="logout" onClick={() => this.logout()}>
                   <Icon
                     type="arrow-right"
                   />
                   Logout
-              {/* </Button> */}
-              </Menu.Item> : <Menu.Item key="logout" style={{ paddingRight: 30, paddingLeft: 20, height: 70, paddingTop: 10 }}>
-                <Icon
-                  type="arrow-right"
-                />
-                Logout
-              </Menu.Item>}
-          </Menu>
+              </Menu.Item> : null}
+            </Menu>
+          </div>
+        </div>
+        <br />
+        <h1 style={{ justifyContent: 'center', textAlign: 'center' }}>News Articles</h1>
+        <br/>
+        <div style={{ width: '100%', justifyContent: 'center', display: 'flex', textAlign: 'center' }}>
+        <Table 
+        // rowSelection={rowSelection} 
+        style={{ width: '94%' }}
+        columns={columns} 
+        dataSource={data} 
+        />
         </div>
       </div>
     )
