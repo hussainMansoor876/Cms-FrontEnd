fb146951eaf8afe73b54aba4e32d45c7774b62bc
Mansoor HU Hussain <hussainmansoor876@gmail.com>
2019-09-24 14:34:07 +0500

    Updated pages1

---
+++ precache-manifest.15a19e5b0a26642fb17a856caab718de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6da0f76ac752563be608682afa60488",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25b35d6d038538b56600",
+    "url": "/static/css/2.7a1db334.chunk.css"
+  },
+  {
+    "revision": "d88369560ccdc5786ced",
+    "url": "/static/css/main.285ab1f7.chunk.css"
+  },
+  {
+    "revision": "25b35d6d038538b56600",
+    "url": "/static/js/2.84ebcf82.chunk.js"
+  },
+  {
+    "revision": "d88369560ccdc5786ced",
+    "url": "/static/js/main.cec64b4f.chunk.js"
+  },
+  {
+    "revision": "6cf167bc38e828ca96a2",
+    "url": "/static/js/runtime~main.b9251d0a.js"
+  }
+]);
