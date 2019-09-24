ac3b96cd14cb405c41ef42267ca4a7a156e6e50c
Mansoor HU Hussain <hussainmansoor876@gmail.com>
2019-09-24 14:33:33 +0500

    Updated pages

--- src/Redux/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import authReducer from './reducers/authReducer'
-
-export default combineReducers({
-    authReducer
-})
--- src/Redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer from './rootReducer'
-import thunk from 'redux-thunk'
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
-
-export default store;
